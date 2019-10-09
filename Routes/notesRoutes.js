319a946a3470e0c7c9e5adc8a5302d0bfed02445
lanners-marshall <lanners.marshall@gmail.com>
2019-10-09 14:56:59 -0700

    update

--- Routes/notesRoutes.js
+++ Routes/notesRoutes.js
@@ -28,7 +28,7 @@ router.post('', (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res.status(500).json(error);
+      return res.status(500).json(error);
     });
 });
 
