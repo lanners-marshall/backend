f241371bb0243320e1d8b17e3f7fe24ff8677786
lanners-marshall <lanners.marshall@gmail.com>
2019-10-16 02:08:00 -0700

    update

--- Routes/commentsRoutes.js
+++ Routes/commentsRoutes.js
@@ -60,6 +60,7 @@ router.delete('/:id', (req, res) => {
   const { id } = req.params;
   db('comments')
     .where({ id })
+    .del()
     .then(response => {
       return res.status(200).json(response);
     })
